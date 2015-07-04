8a55ae2689f941be32fd3204e96028af6606e989
Andrey Petrov <shazow@gmail.com>
2015-07-04 20:37:07 +0200

    It builds!
    Just a red screen, though. And:
    
    2015/07/04 20:35:43 error creating GL program: shader compile: ERROR: 0:12: Use of undeclared identifier 'vert'

--- cube.go
+++ cube.go
@@ -5,22 +5,31 @@ import (
 	"log"
 
 	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/app/debug"
 	"golang.org/x/mobile/event"
-	"golang.org/x/mobile/exp/app/debug"
-	"golang.org/x/mobile/exp/f32"
-	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/f32"
 	"golang.org/x/mobile/geom"
 	"golang.org/x/mobile/gl"
+	"golang.org/x/mobile/gl/glutil"
 )
 
+func Mat2Float(m *f32.Mat4) []float32 {
+	return []float32{
+		m[0][0], m[0][1], m[0][2], m[0][3],
+		m[1][0], m[1][1], m[1][2], m[1][3],
+		m[2][0], m[2][1], m[2][2], m[2][3],
+		m[3][0], m[3][1], m[3][2], m[3][3],
+	}
+}
+
 var (
-	program      gl.Program
-	vertCoord    gl.Attrib
-//	vertTexCoord gl.Attrib
-    projection   gl.Uniform
-    view         gl.Uniform
-    model        gl.Uniform
-	buf          gl.Buffer
+	program   gl.Program
+	vertCoord gl.Attrib
+	//	vertTexCoord gl.Attrib
+	projection gl.Uniform
+	view       gl.Uniform
+	model      gl.Uniform
+	buf        gl.Buffer
 
 	touchLoc geom.Point
 )
@@ -48,7 +57,7 @@ func start() {
 	gl.BufferData(gl.ARRAY_BUFFER, cubeData, gl.STATIC_DRAW)
 
 	vertCoord = gl.GetAttribLocation(program, "vertCoord")
-//	vertTexCoord = gl.GetAttribLocation(program, "vertTexCoord")
+	//	vertTexCoord = gl.GetAttribLocation(program, "vertTexCoord")
 
 	projection = gl.GetUniformLocation(program, "projection")
 	view = gl.GetUniformLocation(program, "view")
@@ -74,30 +83,34 @@ func draw(c event.Config) {
 
 	gl.UseProgram(program)
 
-	gl.UniformMatrix4fv(projection, f32.Perspective(0.785, c.Width/c.Height, 0.1, 10.0))
+	m := &f32.Mat4{}
+	m.Perspective(f32.Radian(0.785), float32(c.Width/c.Height), 0.1, 10.0)
+	gl.UniformMatrix4fv(projection, Mat2Float(m))
 
-	eye := gl.Vec3(3, 3, 3)
-	center := gl.Vec3(0, 0, 0)
-	up := gl.Vec3(0, 1, 0)
-	gl.UniformMatrix4fv(view, f32.Perspective(eye, center, up))
+	eye := f32.Vec3{3, 3, 3}
+	center := f32.Vec3{0, 0, 0}
+	up := f32.Vec3{0, 1, 0}
+	m.LookAt(&eye, &center, &up)
+	gl.UniformMatrix4fv(view, Mat2Float(m))
 
-	gl.UniformMatrix4fv(f32.Identity)
+	m.Identity()
+	gl.UniformMatrix4fv(model, Mat2Float(m))
 
 	gl.BindBuffer(gl.ARRAY_BUFFER, buf)
 
-	gl.EnableVertexAttribArray(position)
-	gl.VertexAttribPointer(position, coordsPerVertex, gl.FLOAT, false, 5, 0)
-//	gl.EnableVertexAttribArray(texture)
-//	gl.VertexAttribPointer(position, texCoordsPerVertex, gl.FLOAT, false, 5, 3)
+	gl.EnableVertexAttribArray(vertCoord)
+	gl.VertexAttribPointer(vertCoord, coordsPerVertex, gl.FLOAT, false, 5, 0)
+	//	gl.EnableVertexAttribArray(texture)
+	//	gl.VertexAttribPointer(vertCoord, texCoordsPerVertex, gl.FLOAT, false, 5, 3)
 
 	gl.DrawArrays(gl.TRIANGLES, 0, vertexCount)
 
-	gl.DisableVertexAttribArray(position)
+	gl.DisableVertexAttribArray(vertCoord)
 
 	debug.DrawFPS(c)
 }
 
-var cubeData = []float32{
+var cubeData = f32.Bytes(binary.LittleEndian,
 	//  X, Y, Z, U, V
 	// Bottom
 	-1.0, -1.0, -1.0, 0.0, 0.0,
@@ -146,12 +159,12 @@ var cubeData = []float32{
 	1.0, -1.0, 1.0, 1.0, 1.0,
 	1.0, 1.0, -1.0, 0.0, 0.0,
 	1.0, 1.0, 1.0, 0.0, 1.0,
-}
+)
 
 const (
-	coordsPerVertex = 3
+	coordsPerVertex    = 3
 	texCoordsPerVertex = 2
-	vertexCount     = 36
+	vertexCount        = 36
 )
 
 const vertexShader = `#version 330
