f168a7cf42281019fb7c1355fd249bcd7510e432
Andrey Petrov <shazow@gmail.com>
2015-07-04 21:18:03 +0200

    Spinny cube!

--- cube.go
+++ cube.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"log"
+	"time"
 
 	"golang.org/x/mobile/app"
 	"golang.org/x/mobile/app/debug"
@@ -32,6 +33,7 @@ var (
 	buf        gl.Buffer
 
 	touchLoc geom.Point
+	started  time.Time
 )
 
 func main() {
@@ -61,6 +63,8 @@ func start() {
 	projection = gl.GetUniformLocation(program, "projection")
 	view = gl.GetUniformLocation(program, "view")
 	model = gl.GetUniformLocation(program, "model")
+
+	started = time.Now()
 }
 
 func stop() {
@@ -77,10 +81,15 @@ func touch(t event.Touch, c event.Config) {
 }
 
 func draw(c event.Config) {
+	since := time.Now().Sub(started)
+
 	gl.ClearColor(0, 0, 0, 1)
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 
 	gl.UseProgram(program)
+
+	identity := &f32.Mat4{}
+	identity.Identity()
 
 	m := &f32.Mat4{}
 	m.Perspective(f32.Radian(0.785), float32(c.Width/c.Height), 0.1, 10.0)
@@ -94,6 +103,7 @@ func draw(c event.Config) {
 	gl.UniformMatrix4fv(view, Mat2Float(m))
 
 	m.Identity()
+	m.Rotate(m, f32.Radian(since.Seconds()), &f32.Vec3{0, 1, 0})
 	gl.UniformMatrix4fv(model, Mat2Float(m))
 
 	gl.BindBuffer(gl.ARRAY_BUFFER, buf)
